ba2f60913e74efbecf7e770d6b1fe1ec14564229
tuisemo <tuisemo@sina.cn>
2017-07-14 18:08:58 +0800

    gulp add extend

--- Ejs_project/gulpfile.js
+++ Ejs_project/gulpfile.js
@@ -2,6 +2,7 @@
 const gulp = require('gulp');
 
 // 引入组件
+const header = require('gulp-header');
 const jshint = require('gulp-jshint');
 const htmlhint = require('gulp-htmlhint');
 const less = require('gulp-less');
@@ -13,88 +14,71 @@ const cache = require("gulp-cache");
 const autoprefixer = require("gulp-autoprefixer");
 
 var banner =
-"/** \n\
+    "/** \n\
 * By 慎独\n\
-* vue项目练习\n \
+*项目练习\n \
 */\n";
 
 // 检查脚本
 gulp.task('jshint', function() {
-    gulp.src('./src/js/*.js')
+    gulp.src('./bulid/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 //检查html
 gulp.task('htmlhint', function() {
-    gulp.src('./src/*.html')
+    gulp.src('./bulid/*.html')
         .pipe(htmlhint())
         .pipe(htmlhint.reporter());
 });
 // 编译Less
 gulp.task('less', function() {
-    gulp.src('./src/css/Style.less')
+    gulp.src('./bulid/css/less/*.less')
         .pipe(cache(less()))
-        .pipe(gulp.dest('./src/css'));
+        .pipe(gulp.dest('./bulid/css'));
 });
 //补全前缀+压缩css
 gulp.task('cssmin', function() {
-    gulp.src(['./src/css/normalize.css', './src/css/layer.css', './src/css/unslider.css','./src/css/webuploader.css'])
-        .pipe(cache(cssmin()))
-        .pipe(gulp.dest('./dist/css'));
-    gulp.src(['./src/css/bootstrapStyle.css', './src/css/Style.css'])
+    gulp.src(['./bulid/css/*.css'])
         .pipe(autoprefixer({
             browsers: ['last 4 versions']
                 //cascade: true, //是否美化属性值 默认：true 像这样：
                 //remove: true //是否去掉不必要的前缀 默认：true 
         }))
         .pipe(cache(cssmin()))
-        .pipe(concat('PassportStyle.css'))
-        .pipe(gulp.dest('./src/css'))
-        .pipe(gulp.dest('./dist/css'));
+        .pipe(gulp.dest('./public/css'));
 });
 //include公共文件
 gulp.task('fileinclude', /* ['less', 'cssmin', 'scripts'],*/ function() {
-    gulp.src('./src/*.html')
-        .pipe(fileinclude({
-            prefix: '@@',
-            basepath: '@file',
-            indent: true
-        }))
-        .pipe(inject(gulp.src(['./src/css/normalize.css', './src/css/PassportStyle.css'], { reda: false }), { starttag: '<!-- inject:base:{{ext}} -->', relative: true }))
-        //.pipe(inject(gulp.src(['./src/js/MSG.js', './src/js/lib/jquery.js'], { reda: false }), { starttag: '<!-- inject:base:{{ext}} -->', relative: true }))
-        //.pipe(inject(gulp.src(['./src/js/lib/vue.js'], { reda: false }), { starttag: '<!-- inject:vue:{{ext}} -->', relative: true }))
-        .pipe(inject(gulp.src(['./src/js/lib/require.js'], { reda: false }), { starttag: '<!-- inject:require:{{ext}} -->', relative: true }))
+    gulp.src('./bulid/*.html')
+        .pipe(inject(gulp.src(['./bulid/js/lib/CommonJS.js'], { reda: false }), { starttag: '<!-- inject:require:{{ext}} -->', relative: true }))
         .pipe(fileinclude({
             prefix: '<!--IEhack@',
             suffix: '-->',
             basepath: '@file',
             indent: true
         }))
-        .pipe(htmlbeautify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('public'));
 });
 
 // 合并，压缩文件
 gulp.task('scripts', function() {
-    gulp.src(['./src/js/*.js'])
-        //.pipe(jsmin())
-        .pipe(gulp.dest('./dist/js'));
-    /*gulp.src(['./src/js/lib/lodash.js'])
+    gulp.src(['./bulid/js/*.js'])
         .pipe(jsmin())
-        .pipe(gulp.dest('./dist/js/lib'));*/
-    gulp.src(['./src/js/lib/*.js', './src/js/lib/WebUploader.js']) //库文件不再压缩
-        //.pipe(jsmin())
-        .pipe(gulp.dest('./dist/js/lib'));
+        .pipe(gulp.dest('./public/js'));
+});
+
+// 文件迁移
+gulp.task('copy', function() {
+    gulp.src(['./bulid/js/lib/*.js'])
+        .pipe(gulp.dest('./public/js/lib'));
 });
 
 // 默认任务
-gulp.task('default', ['cssmin', 'jshint', 'scripts', 'fileinclude', 'imagemin'], function() {
-    //gulp.run('cssmin', 'jshint', 'scripts', 'fileinclude', 'imagemin');
-
+gulp.task('default', ['jshint', 'cssmin', 'scripts', 'fileinclude'], function() {
     // 监听文件变化
-    gulp.watch('./src/js/*.js', ['jshint', 'scripts']);
-    gulp.watch('./src/css/*.less', ['less']);
-    gulp.watch('./src/css/*.css', ['cssmin']);
-    gulp.watch(['./src/*.html'], ['fileinclude']);
-    gulp.watch(['./src/include/*.html'], ['fileinclude','htmlminify', 'htmlbeautify']);
+    gulp.watch('./bulid/js/*.js', ['jshint', 'scripts']);
+    gulp.watch('./bulid/css/less/*.less', ['less']);
+    gulp.watch('./bulid/css/*.css', ['cssmin']);
+    gulp.watch(['./bulid/*.html'], ['htmlhint', 'fileinclude']);
 });
