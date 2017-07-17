e1d8f358923187bd70c0ffbf822db378b7b47ce1
tuisemo <tuisemo@sina.cn>
2017-07-17 17:10:27 +0800

    gulp task

--- Ejs_project/gulpfile.js
+++ Ejs_project/gulpfile.js
@@ -37,34 +37,36 @@ gulp.task('htmlhint', function() {
 });
 // 编译Less
 gulp.task('less', function() {
-    gulp.src('./bulid/css/less/*.less')
+    var lessbulid = gulp.src('./bulid/css/less/*.less')
         .pipe(cache(less()))
-        //.pipe(cache(concat('PassportCss.css')))
         .pipe(gulp.dest('./bulid/css/stylesheets'));
+    return lessbulid;
 });
-// 合并样式文件
-gulp.task('concat', function() {
-    gulp.src(['./bulid/css/stylesheets/*.min.css'])
-        .pipe(concat('PassportCss.min.css'))
-        .pipe(gulp.dest('./bulid/css'));
-});
-
 //补全前缀+压缩css
-gulp.task('cssmin', function() {
-    gulp.src(['./bulid/css/stylesheets/*.css','!./bulid/css/stylesheets/*.min.css'])
+gulp.task('cssmin', ['less'], function() {
+    var cssminbulid = gulp.src(['./bulid/css/stylesheets/*.css', '!./bulid/css/stylesheets/*.min.css'])
         .pipe(autoprefixer({
             browsers: ['last 4 versions']
-                //cascade: true, //是否美化属性值 默认：true 像这样：
-                //remove: true //是否去掉不必要的前缀 默认：true 
+            //cascade: true, //是否美化属性值 默认：true 像这样：
+            //remove: true //是否去掉不必要的前缀 默认：true 
         }))
         .pipe(cache(cssmin()))
         .pipe(ext_replace('.min.css'))
         .pipe(gulp.dest('./bulid/css/stylesheets'));
+    return cssminbulid;
 });
+// 合并样式文件
+gulp.task('concat', ['cssmin'], function() {
+    var concatbulid = gulp.src(['./bulid/css/stylesheets/*.min.css'])
+        .pipe(concat('PassportCss.min.css'))
+        .pipe(gulp.dest('./bulid/css'));
+    return concatbulid;
+});
+
 //include公共文件
-gulp.task('fileinclude', /* ['less', 'cssmin', 'scripts'],*/ function() {
+gulp.task('fileinclude', ['concat'], function() {
     gulp.src('./bulid/*.html')
-        .pipe(inject(gulp.src(['bulid/css/*.css'/*,'./bulid/css/PassportCss.css'*/], { reda: false }), { starttag: '<!-- inject:base:{{ext}} -->', relative: true }))
+        .pipe(inject(gulp.src(['./bulid/css/*.css'], { reda: false }), { starttag: '<!-- inject:base:{{ext}} -->', relative: true }))
         .pipe(inject(gulp.src(['./bulid/js/lib/CommonJS.js'], { reda: false }), { starttag: '<!-- inject:CommonJS:{{ext}} -->', relative: true }))
         .pipe(fileinclude({
             prefix: '<!--IEhack@',
@@ -84,7 +86,7 @@ gulp.task('scripts', function() {
 });
 
 // 文件迁移
-gulp.task('copy', function() {
+gulp.task('copy', ['concat'], function() {
     gulp.src(['./bulid/js/lib/*.js'])
         .pipe(gulp.dest('./public/js/lib'));
 
@@ -93,12 +95,12 @@ gulp.task('copy', function() {
 });
 
 // 默认任务
-gulp.task('default', ['jshint','less', 'cssmin','scripts', 'concat','fileinclude','copy'], function() {
+gulp.task('default', ['jshint', 'concat', 'scripts', 'fileinclude', 'copy'], function() {
     // 监听文件变化
     gulp.watch('./bulid/js/*.js', ['jshint', 'scripts']);
-    gulp.watch('./bulid/js/lib/*.js', ['jshint','copy']);
+    gulp.watch('./bulid/js/lib/*.js', ['jshint', 'copy']);
     gulp.watch('./bulid/css/less/*.less', ['less']);
-    gulp.watch('./bulid/css/**/*.css', ['cssmin','copy']);
-    gulp.watch(['./bulid/*.html'], ['fileinclude','htmlhint']);
-    //gulp.watch(['./public/*.html'], ['']);
+    gulp.watch('./bulid/css/*/*.css', ['cssmin', 'concat', 'copy']);
+    gulp.watch('./bulid/css/*.css', ['cssmin', 'concat', 'copy']);
+    gulp.watch(['./bulid/*.html'], ['fileinclude', 'htmlhint']);
 });
