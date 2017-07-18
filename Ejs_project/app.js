5ccd6cefc2f311f5258b17d4dbcc4b652a9f1f14
tuisemo <tuisemo@sina.cn>
2017-07-18 18:31:45 +0800

    gulp add

--- Ejs_project/app.js
+++ Ejs_project/app.js
@@ -5,12 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var app = express();
-
 var index = require('./routes/index');
 var users = require('./routes/users');
 var hack = require('./routes/hack');
 
+var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views')); //设置了模版文件夹的路径
 //app.set('view engine', 'ejs');//设置使用的模板引擎，此处设置为ejs
@@ -23,33 +22,31 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use('/', index);//该指引必须在静态资源目录指定之前
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
 app.use('/users', users);
 app.use('/hack', hack);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-
 //listen
 app.listen(8080, function() {
     console.log("ejs Server Start!");
 });
-
 module.exports = app;
